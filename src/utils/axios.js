1ac9ca55e8da03caf4e5b002bcba6f64bc571a18
Damilare <da9783790@gmil.com>
2025-07-13 17:18:54 +0100

    upadet axios.js commit

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://retina-detect-backend.vercel.app',
+  baseURL: 'https://retina-detect-backend-git-main-damilare-projects.vercel.app',
   withCredentials: true,
 });
 
